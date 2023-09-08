04d3d640527ef2fe57908174676ce7269463f161
Christopher Partin <christopherpartin11@gmail.com>
2023-09-08 12:00:09 -0700

    Added docstrings and comments to main.py, and added 3 functions for the 3 different censorship options in censor_script.py to create structure, along with their docstrings. Next I will implement better_profanity into the first censorship option and test it.

--- censor_script.py
+++ censor_script.py
@@ -1,1 +1,32 @@
 from better_profanity import profanity
+
+
+
+def replace_with_asterisk(text_file):
+    """
+    Uses the better-profanity package to replace each instance of each word with asterisks and then makes a new file
+    :param: text_file: Txt (File to be censored)
+    :return:
+    """
+    pass
+
+
+def replace_with_less_offensive(text_file):
+    """
+    Uses a dictionary with a variety of curse words as keys and their less offensive versions as values and replaces
+    them, before making a new text file
+    :param: text_file: Txt (File to be censored)
+    :return:
+    """
+    pass
+
+
+def replace_with_funny(text_file):
+    """
+    Uses a dictionary with a variety of curse words as keys and their funny versions as values and replaces them,
+    before making a new text file
+    :param: text_file: Txt (File to be censored)
+    :return:
+    """
+    pass
+
--- main.py
+++ main.py
@@ -7,22 +7,30 @@ def main():
           "3) Replace each swear word with a funny, less offensive version (\"You're a poop head\")")
 
     def choose_option():
+        """
+        Asks the user for inputs to determine which censor option they want, and double-checks that they typed the
+        correct one. Has edge cases for typos or options out of range.
+        :return: censor_option: String (number selection 1-3)
+        """
         censor_option = input("Please type the corresponding number to your preferred option above. \n")
 
         if censor_option == "1" or censor_option == "2" or censor_option == "3":
             correct_option = input("You selected " + censor_option + ". " +
                                    "Is that correct? Type Y for yes, and N for No \n")
 
+            # if option is No or neither Yes nor No, ask again
             if correct_option.upper() == "N" or (correct_option.upper() != "Y" and correct_option.upper() != "N"):
                 choose_option()
 
             return censor_option
 
         else:
+            # If user types in anything other than 1, 2, or 3, ask again.
             censor_option = choose_option()
 
     user_choice = choose_option()
 
+    # Current
     if user_choice == "1":
         print("You chose 1!")
 
